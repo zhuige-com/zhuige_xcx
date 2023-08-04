4fa495d351d7d46ec36ad9e707e1642887099ab7
longwenjunjie <longwenjunj@163.com>
2023-08-04 09:28:11 +0800

    fix

--- zhuige-xcx/addons/zhuige-bbs/function.php
+++ zhuige-xcx/addons/zhuige-bbs/function.php
@@ -76,6 +76,7 @@ if (!function_exists('zhuige_bbs_topic_format')) {
 		// 位置信息
 		$item['location'] = $options['location'];
 
+		$item['comments'] = [];
 		$bbs_list_comment = ZhuiGe_Xcx::option_value('bbs_list_comment');
 		if ($bbs_list_comment && isset($bbs_list_comment['switch']) && $bbs_list_comment['switch']) {
 			$item['comments'] = zhuige_xcx_get_comments($post->ID, 0, $bbs_list_comment['count']);
